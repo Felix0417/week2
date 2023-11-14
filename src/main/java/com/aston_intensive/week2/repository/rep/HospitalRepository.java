cd24b381eb6f43652836271fad39bd871b16d226
Felix0417 <felix.the.cat.msk@gmail.com>
2023-11-15 04:40:41 +0500

    .

--- src/main/java/com/aston_intensive/week2/repository/rep/HospitalRepository.java
+++
@@ -1,6 +0,0 @@
-package com.aston_intensive.week2.repository.rep;
-
-import com.aston_intensive.week2.model.Hospital;
-
-public interface HospitalRepository extends Repository<Hospital, Integer> {
-}
